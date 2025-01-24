f3cee62625993058e13b3527e5b42adfdd853d42
Hugo Pinchemel <148193347+Hugopinchemel@users.noreply.github.com>
2025-01-24 02:39:51 +0100

    Add interactive scripts for user flow in 2pype and remove legacy script

---
+++ script/2pype-paul.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const nextBtn = document.querySelector('.next-btn');
+  const rightSide1 = document.querySelector('.right-side-1');
+  const rightSide2 = document.querySelector('.right-side-2');
+  const call = document.querySelector('.call');
+  const btnTrustPaul = document.getElementById('btn-trust-paul');
+  const btnNoTrustPaul = document.getElementById('btn-no-trust-paul');
+  const alertTrust = document.getElementById('trust');
+  const alertPaul = document.getElementById('Alert-Paul');
+
+  nextBtn.addEventListener('click', () => {
+    rightSide1.style.display = 'none';
+    rightSide2.style.display = 'none';
+    call.style.display = 'grid';
+  });
+
+  btnTrustPaul.addEventListener('click', () => {
+    alertTrust.style.display = 'block';
+  });
+
+  btnNoTrustPaul.addEventListener('click', () => {
+    alertPaul.style.display = 'block';
+  });
+});
