e51f045d39c44bd319e09750d4e4cfe4bf877b11
Lucas33860 <150592389+Lucas33860@users.noreply.github.com>
2025-01-24 10:20:21 +0100

    glossaire end

--- script/glossaire.js
+++ script/glossaire.js
@@ -1,21 +1,63 @@
-document.querySelectorAll(".page4, .page3").forEach(function (div) {
-  div.style.display = "none";
-});
-
-document.getElementById("suivant").addEventListener("click", function () {
-  document.querySelectorAll(".page1, .page2").forEach(function (div) {
+document
+  .querySelectorAll(" .page3, .page4, .page5, .page6, .page7, .page8")
+  .forEach(function (div) {
     div.style.display = "none";
   });
-  document.querySelectorAll(".page3, .page4").forEach(function (div) {
-    div.style.display = "block";
-  });
+
+let currentPage = 1;
+
+document.getElementById("suivant").addEventListener("click", function () {
+  if (currentPage === 1) {
+    document.querySelectorAll(".page1, .page2").forEach(function (div) {
+      div.style.display = "none";
+    });
+    document.querySelectorAll(".page3, .page4").forEach(function (div) {
+      div.style.display = "block";
+    });
+    currentPage = 2;
+  } else if (currentPage === 2) {
+    document.querySelectorAll(".page3, .page4").forEach(function (div) {
+      div.style.display = "none";
+    });
+    document.querySelectorAll(".page5, .page6").forEach(function (div) {
+      div.style.display = "block";
+    });
+    currentPage = 3;
+  } else if (currentPage === 3) {
+    document.querySelectorAll(".page5, .page6").forEach(function (div) {
+      div.style.display = "none";
+    });
+    document.querySelectorAll(".page7, .page8").forEach(function (div) {
+      div.style.display = "block";
+    });
+    currentPage = 4;
+  }
 });
 
 document.getElementById("precedent").addEventListener("click", function () {
-  document.querySelectorAll(".page3, .page4").forEach(function (div) {
-    div.style.display = "none";
-  });
-  document.querySelectorAll(".page1, .page2").forEach(function (div) {
-    div.style.display = "block";
-  });
+  if (currentPage === 2) {
+    document.querySelectorAll(".page3, .page4").forEach(function (div) {
+      div.style.display = "none";
+    });
+    document.querySelectorAll(".page1, .page2").forEach(function (div) {
+      div.style.display = "block";
+    });
+    currentPage = 1;
+  } else if (currentPage === 3) {
+    document.querySelectorAll(".page5, .page6").forEach(function (div) {
+      div.style.display = "none";
+    });
+    document.querySelectorAll(".page3, .page4").forEach(function (div) {
+      div.style.display = "block";
+    });
+    currentPage = 2;
+  } else if (currentPage === 4) {
+    document.querySelectorAll(".page7, .page8").forEach(function (div) {
+      div.style.display = "none";
+    });
+    document.querySelectorAll(".page5, .page6").forEach(function (div) {
+      div.style.display = "block";
+    });
+    currentPage = 3;
+  }
 });
